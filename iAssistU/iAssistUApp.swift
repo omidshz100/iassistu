f47c1be1702f053db9a3e6af813ce0ff92587a89
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-07-01 19:49:15 +0200

    Initial Commit

---
+++ iAssistU/iAssistUApp.swift
@@ -0,0 +1,17 @@
+//
+//  iAssistUApp.swift
+//  iAssistU
+//
+//  Created by Omid Shojaeian Zanjani on 01/07/23.
+//
+
+import SwiftUI
+
+@main
+struct iAssistUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
